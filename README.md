4a0e8f45173759bba05fae200b8a545f0612c307
oyu <oscar@sensortransport.com>
2022-09-13 18:37:48 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # stream2multipart
+
+Receive stream chunks and convert it into an easy to use object array.
+
+Each element in the array is a section of part of the multipart download response.
+
+## Quick start
+
+npm install stream2multipart
+
+````
+const Stream2MultiPart = require('stream2multipart');
+
+const axios = require('axios');
+const fs = require('fs');
+const { StreamToBuffer } = require('./Stream2Buffer');
+
+const config = {
+    method: 'get',
+    url: 'https://yourServerUrl',
+    headers: {
+        Authorization: 'Bearer add authentication and any other headers',
+    },
+    responseType: 'stream'  // This is important!
+};
+
+const path = '/Users/oscaryu/Downloads/';
+
+axios(config).then((res) => {
+    console.log(res)
+    // res.data.pipe(fs.createWriteStream(`${path}public-test.pdf`)); // dump response data to file
+    StreamToBuffer(res).then((parts) => {
+        if (parts && parts.length) {
+            parts.forEach((part) => {
+                if (part.Filename) {
+                    const wstream = fs.createWriteStream(`${path}${part.Filename}`);
+                    wstream.write(part.Content);
+                    wstream.end();
+                    console.log(`Saved ${path}${part.Filename}`);
+                } else if (part.ContentType === 'application/json') {
+                    console.log(JSON.stringify(part.Content, null, 4));
+                } else {
+                    console.log(part.Content);
+                }
+            });
+        }
+    });
+}).catch((err) => {
+    console.log(err);
+});
+
+````
+
+
+UnhandledPromiseRejectionWarning: Error: Sheet "MEX" not found in the *.xlsx file. Available sheets: "USA" (#1), "CAN" (#2).
---
+++ index.js
@@ -0,0 +1,78 @@
+function getBoundary(res) {
+    const header = res.headers['content-type'];
+    const items = header.split(';');
+    if (items) {
+        const item = items.filter((i) => i.indexOf('boundary') >= 0);
+        if (item && item.length) {
+            const k = item[0].split('=');
+            return `--${k[1].trim().replace(/^["']|["']$/g, '')}`;
+        }
+    }
+    return '';
+}
+
+function StreamToBuffer(res) {
+    const stream = res.data;
+    const chunks = [];
+    return new Promise((resolve, reject) => {
+        stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+        stream.on('error', (err) => reject(err));
+        stream.on('end', () => {
+            const boundary = getBoundary(res);
+            console.log('boundary', boundary);
+            const buffer = Buffer.concat(chunks);
+            // console.log('buffer', buffer.toString());
+            let lastPos = 0;
+            let pos = buffer.indexOf('\r\n');
+            const data = [];
+            const parts = [];
+
+            let isContentReady = false;
+            let sectionDisposition = '';
+            let sectionType = '';
+            let sectionData = '';
+            let part = {};
+            for (let i = 1; lastPos < buffer.length; i++) {
+                pos = buffer.indexOf('\r\n', lastPos);
+                const section = buffer.slice(lastPos, pos);
+                lastPos = pos + 2;
+                if (section.toString().trim().startsWith(boundary)) {
+                    if (lastPos < pos + 3) {
+                        // --------- Done ---------
+                        resolve(parts);
+                    } else {
+                        // --------- New Section ---------
+                        isContentReady = false;
+                        part = {};
+                    }
+                } else if (section.toString() === '') {
+                    // --------- Data is next ---------
+                    isContentReady = true;
+                } else if (isContentReady) {
+                    sectionData = section;
+                    try {
+                        part.Content = part.ContentType === 'application/json' ? JSON.parse(section) : section;
+                    } catch (err) {
+                        console.log(part, section.toString().substring(0, 100), err);
+                    }
+                    parts[parts.length] = { ...part };
+                    sectionType = '';
+                    sectionDisposition = '';
+                    isContentReady = false;
+                } else if (section.toString().startsWith('Content-Type:')) {
+                    sectionType = section.toString().slice(13, section.length).trim();
+                    part.ContentType = sectionType;
+                } else if (section.toString().startsWith('Content-Disposition:')) {
+                    sectionDisposition = section.toString().slice(20, section.length).trim();
+                    part.Disposition = sectionDisposition;
+                    if (sectionDisposition.indexOf('filename=') >= 0) {
+                        [, part.Filename] = sectionDisposition.split('filename=');
+                        part.Filename = part.Filename.replace(/["']/g, '');
+                    }
+                }
+            }
+        });
+    });
+}
+
+module.exports.StreamToBuffer = StreamToBuffer;
